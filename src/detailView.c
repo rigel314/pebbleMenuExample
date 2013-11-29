1ccd208286a8692d57d8be744b5c9de2221f15a7
Cody Creager <pi.rubiks@gmail.com>
2013-11-28 21:40:31 -0500

    Updating to 2.0.

--- src/detailView.c
+++ src/detailView.c
@@ -1,30 +1,35 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-typedef int (*sprintfref)(char *restrict s, const char *restrict format, ...);
-
-Window detailW;
-TextLayer detailW_text;
+Window* detailW;
+TextLayer* detailW_text;
 
 char msg[10+1+6+1];
 extern char hex[]; // Defined in menuExample.c
 
+void detailUnload(Window* win)
+{
+	APP_LOG(APP_LOG_LEVEL_INFO, "Destroying Detail Window.");
+	text_layer_destroy(detailW_text);
+	window_destroy(detailW);
+}
+
 void showDetail(MenuIndex* index)
 {
-	window_init(&detailW, "Menu Exapmle");
+	WindowHandlers wh = { .unload = &detailUnload };
+	detailW = window_create();
+	window_set_window_handlers(detailW, wh);
 
-	text_layer_init(&detailW_text, GRect(0,52,144,40));
-	text_layer_set_text_alignment(&detailW_text, GTextAlignmentCenter); // Center the text.
-	text_layer_set_font(&detailW_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+	detailW_text = text_layer_create(GRect(0,52,144,40));
+	text_layer_set_text_alignment(detailW_text, GTextAlignmentCenter); // Center the text.
+	text_layer_set_font(detailW_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	msg[0] = 0; // Ensure the message starts with a null so strcat will overwrite it.
 	strcat(msg, "Section: ");
 	strcat(msg, hex+index->section*2);
 	strcat(msg, "\nRow: ");
 	strcat(msg, hex+index->row*2);
 
-	text_layer_set_text(&detailW_text, msg);
-	layer_add_child(&detailW.layer, &detailW_text.layer);
+	text_layer_set_text(detailW_text, msg);
+	layer_add_child(window_get_root_layer(detailW), text_layer_get_layer(detailW_text));
 
-	window_stack_push(&detailW, true); // The back button will dismiss the current window, not close the app.  So just press back to go back to the master view.
+	window_stack_push(detailW, true); // The back button will dismiss the current window, not close the app.  So just press back to go back to the master view.
 }
--- src/menuExample.c
+++ src/menuExample.c
@@ -1,23 +1,14 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-
-#define MY_UUID { 0x2A, 0xC7, 0xAC, 0xEB, 0xB6, 0xE2, 0x45, 0x39, 0x9E, 0xC2, 0xB9, 0x8A, 0xA6, 0x88, 0x32, 0xC9 }
-PBL_APP_INFO(MY_UUID,
-			 "Menu Example", "Computing Eureka",
-			 1, 0, /* App version */
-			 DEFAULT_MENU_ICON,
-			 APP_INFO_STANDARD_APP);
-
-Window window;
-MenuLayer mainMenu;
+Window* window;
+MenuLayer* mainMenu;
 void showDetail(MenuIndex* index); // Defined in detailView.c
 
 char hex[] = "0\0001\0002\0003\0004\0005\0006\0007\0008\0009\000A\000B\000C\000D\000E\000F";
 // hex+2*0 is the string "0",
 // hex+2*1 is the string "1",
-// hex+2*3 is the string "3" and so on.  There is an implied \0 at the end of a constant c string.
+// hex+2*3 is the string "3" and so on.  There is an implied \0 at the end of a constant C string.
+// I used \000 here to escape a null-termination.  \0 followed by a 1 looks like \01 and confuses my syntax hilighter.  It may also confuse the compiler.
 
 void mainMenu_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context)
 { // Show the detail view when select is pressed.
@@ -26,13 +17,13 @@ void mainMenu_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, 
 void mainMenu_draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *callback_context)
 { // Adding the row number as text on the row cell.
 	graphics_context_set_text_color(ctx, GColorBlack); // This is important.
-	graphics_text_draw(ctx, hex+2*cell_index->row, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(0,0,cell_layer->frame.size.w,cell_layer->frame.size.h), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
-	// Just saying cell_layer->frame for the 4th argument doesn't work.  Probably because the GContext is relative to the cell already, but the cell_layer.frame is relative to the menulayer or the screen or something.
+	graphics_draw_text(ctx, hex+2*cell_index->row, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(0,0,layer_get_frame(cell_layer).size.w,layer_get_frame(cell_layer).size.h), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+	// Just saying layer_get_frame(cell_layer) for the 4th argument doesn't work.  Probably because the GContext is relative to the cell already, but the cell_layer.frame is relative to the menulayer or the screen or something.
 }
 void mainMenu_draw_header(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *callback_context)
 { // Adding the header number as text on the header cell.
 	graphics_context_set_text_color(ctx, GColorBlack); // This is important.
-	graphics_text_draw(ctx, hex+2*section_index, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(0,0,cell_layer->frame.size.w,cell_layer->frame.size.h), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+	graphics_draw_text(ctx, hex+2*section_index, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD), GRect(0,0,layer_get_frame(cell_layer).size.w,layer_get_frame(cell_layer).size.h), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
 }
 int16_t mainMenu_get_header_height(struct MenuLayer *menu_layer, uint16_t section_index, void *callback_context)
 { // Always 30px tall for a header cell
@@ -51,15 +42,19 @@ uint16_t mainMenu_get_num_sections(struct MenuLayer *menu_layer, void *callback_
 	return 3;
 }
 
-MenuLayerCallbacks cbacks;
+MenuLayerCallbacks cbacks; // The Pebble API documentation says this should be long-lived
+// Currently, however, MenuLayerCallbacks is a struct and the menu_layer_set_callbacks does not take a pointer to this struct as an argument.
+// Instead, it takes the struct itself.  In C structs are passed by value and returned by value, so this would work fine if it were not long-lived.  The struct's data would be copied anyway.
 
-void handle_init(AppContextRef ctx) {
-	(void)ctx;
+void init(void)
+{
+	GRect frame;
+	window = window_create();
 
-	window_init(&window, "Menu Exapmle");
+	frame = layer_get_bounds(window_get_root_layer(window));
 
-	menu_layer_init(&mainMenu, GRect(0,0,window.layer.frame.size.w,window.layer.frame.size.h-15)); // -15 because of the title bar.  I could go full screen, but opted not to.
-	menu_layer_set_click_config_onto_window(&mainMenu, &window); // I assume this sets the Window's button callbacks to the MenuLayer's default button callbacks.
+	mainMenu = menu_layer_create(GRect(0,0,frame.size.w,frame.size.h));
+	menu_layer_set_click_config_onto_window(mainMenu, window); // Sets the Window's button callbacks to the MenuLayer's default button callbacks.
 	// Set all of our callbacks.
 	cbacks.get_num_sections = &mainMenu_get_num_sections; // Gets called at the beginning of drawing the table.
 	cbacks.get_num_rows = &mainMenu_get_num_rows_in_section; // Gets called at the beginning of drawing each section.
@@ -70,16 +65,21 @@ void handle_init(AppContextRef ctx) {
 	cbacks.draw_header = &mainMenu_draw_header; // Gets called to set the content of a header cell.
 	// cbacks.selection_changed = &func(struct MenuLayer *menu_layer, MenuIndex new_index, MenuIndex old_index, void *callback_context) // I assume this would be called whenever an up or down button was pressed.
 	// cbacks.select_long_click = &func(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) // I didn't use this.
-	menu_layer_set_callbacks(&mainMenu, NULL, cbacks); // I have no idea what the middle argumnte, void *callback_context, is for.  It seems to work fine with NULL there.
-	layer_add_child(&window.layer, menu_layer_get_layer(&mainMenu));
+	menu_layer_set_callbacks(mainMenu, NULL, cbacks); // I have no user data to supply to the callback functions, so 
+	layer_add_child(window_get_root_layer(window), menu_layer_get_layer(mainMenu));
 
-	window_stack_push(&window, true);
+	window_stack_push(window, true);
 }
 
+void deinit(void)
+{
+	menu_layer_destroy(mainMenu);
+	window_destroy(window);
+}
 
-void pbl_main(void *params) {
-	PebbleAppHandlers handlers = {
-		.init_handler = &handle_init
-	};
-	app_event_loop(params, &handlers);
+int main(void)
+{
+	init();
+	app_event_loop();
+	deinit();
 }
